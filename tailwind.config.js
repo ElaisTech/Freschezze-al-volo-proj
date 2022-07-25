57c2a85bce458547481922c1747f10cd3750408d
ElaisTech <86303399+ElaisTech@users.noreply.github.com>
2022-07-25 10:58:48 +0200

    changed graphics
    Graphics to png, added backgrounds. Fixed css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./Project/azienda.html", "./Project/Home.html", "./Project/contatti.html", "./Project/prodotti.html", "./Project/index.js"],
+  content: ["./Project/azienda.html", "./Project/index.html", "./Project/contatti.html", "./Project/prodotti.html", "./Project/index.js"],
   theme: {
     extend: {},
   },
